78f2b7628d35cfd77f821d5914cd5b58d6ae7ea7
StarsHackerMC <StarsUsage@163.com>
2022-09-26 19:24:48 +0800

    Fix MixinItemRenderer.java

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/Animations.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/Animations.kt
@@ -17,7 +17,7 @@ object Animations : Module() {
         "BlockingMode", arrayOf(
             "Akrien", "Avatar", "ETB", "Exhibition", "Push", "Reverse",
             "Shield", "SigmaNew", "SigmaOld", "Slide", "SlideDown", "HSlide", "Swong", "VisionFX",
-            "Swank", "Jello", "Rotate", "Liquid",
+            "Swank", "Jello", "Rotate", "Liquid", "Yeet", "Yeet2",
             "None"
         ),
         "SlideDown"
@@ -34,6 +34,9 @@ object Animations : Module() {
     val translateXValue = FloatValue("TranslateX", 0.0f, 0.0f, 1.5f)
     val translateYValue = FloatValue("TranslateY", 0.0f, 0.0f, 0.5f)
     val translateZValue = FloatValue("TranslateZ", 0.0f, 0.0f, -2.0f)
+    val rotateXValue = FloatValue("RotateX", 0.0f, -180f, 180f)
+    val rotateYValue = FloatValue("RotateY", 0.0f, -180f, 180f)
+    val rotateZValue = FloatValue("RotateZ", 0.0f, -180f, 180f)
     val itemPosXValue = FloatValue("ItemPosX", 0.56F, -1.0F, 1.0F)
     val itemPosYValue = FloatValue("ItemPosY", -0.52F, -1.0F, 1.0F)
     val itemPosZValue = FloatValue("ItemPosZ", -0.71999997F, -1.0F, 1.0F)
